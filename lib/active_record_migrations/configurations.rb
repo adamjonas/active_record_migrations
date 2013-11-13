5ac82ca090e1911b5d6a19d8b4d29f39cd0e94a4
Adam Jonas <adam.jonas@carrotcreative.com>
2013-11-13 16:44:47 -0500

    Update configurations.rb

--- lib/active_record_migrations/configurations.rb
+++ lib/active_record_migrations/configurations.rb
@@ -8,7 +8,7 @@ module ActiveRecordMigrations
     attr_accessor :yaml_config, :database_configurations, :environment, :db_dir, :migrations_paths, :schema_format, :seed_loader
 
     def initialize
-      @yaml_config = 'db/config.yml'
+      @yaml_config = 'config/database.yml'
       @environment = ENV['db'] || Rails.env
       @db_dir = 'db'
       @migrations_paths = ['db/migrate']
